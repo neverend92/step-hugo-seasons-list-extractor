55ba2cceca3109e31bf06bdb31bb5058116087c5
Stefan Hettich <stefan.hettich@devoteam.com>
2017-09-03 17:52:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# hugo-seasons-list-extractor
+
+## Usage
+```
+build:
+  steps:
+    - neverend92/hugo-seasons-list-extractor
+        path: http://<path-to-file>/file.html
+        name: Stefan Hettich
+        disciplines: 800m,1500m,One Mile,3000m
+        output_file: data/seasonsList.json
+
+```
---
+++ run.sh
@@ -0,0 +1,23 @@
+if [ ! -n "${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_PATH} "]
+then
+    fail "missing option \"path\", aborting"
+fi
+if [ ! -n "${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_NAME} "]
+then
+    fail "missing option \"name\", aborting"
+fi
+if [ ! -n "${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_DISCIPLINES} "]
+then
+    fail "missing option \"disciplines\", aborting"
+fi
+if [ ! -n "${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_OUTPUT_FILE} "]
+then
+    fail "missing option \"output_file\", aborting"
+fi
+
+PATH=${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_PATH}
+NAME=${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_NAME}
+DISCIPLINES=${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_DISCIPLINES}
+OUTPUT_FILE=${WERCKER_HUGO_SEASONS_LIST_EXTRACTOR_OUTPUT_FILE}
+
+hugo-seasons-list-extractor --path=${PATH} --name=${NAME} --disciplines=${DISCIPLINES} --outputFile=${OUTPUT_FILE}
